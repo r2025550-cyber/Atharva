4b3f4163cd9a68ea43957ccf56d3ca7ca5a5721a
r2025550-cyber <r2025550@gmail.com>
2025-09-26 18:04:27 +0530

    Delete src directory

--- src/utils/logger.py
+++
@@ -1,6 +0,0 @@
-from loguru import logger
-import sys, os
-
-level = os.getenv("LOG_LEVEL", "INFO").upper()
-logger.remove()
-logger.add(sys.stdout, level=level, enqueue=True, backtrace=False, diagnose=False)
